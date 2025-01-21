a13c7256dfc7a57672e68f65c9a779d4bd2adaeb
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2025-01-21 18:14:39 +0000

    complete basic setup

---
+++ exhibition-curator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <h1>Exhibition Curation Platform</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
