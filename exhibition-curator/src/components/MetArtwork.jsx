db8ad854223afcf0854c6c57387c70652a03d8fa
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2025-01-22 14:55:33 +0000

    add components for home page and second api display

--- exhibition-curator/src/components/MetArtwork.jsx
+++ exhibition-curator/src/components/MetArtwork.jsx
@@ -14,12 +14,15 @@ export const MetArtwork = () => {
         {isLoading && <p>Loading artworks...</p>}
         {error && <p>Error fetching artworks</p>}
         {data && data.objectIDs && (
+        <>
+        <h1>Metropolitan Museum of Art</h1>
         <div className="artwork-list">
           {/* Loop through the objectIDs and fetch artwork details */}
           {data.objectIDs.slice(0,50).map((id) => (
-            <ArtworkCard key={id} id={id} />
-          ))}
+              <ArtworkCard key={id} id={id} />
+            ))}
         </div>
+        </>
       )}
         </>
     );
