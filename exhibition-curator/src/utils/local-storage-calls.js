d4cf7a6040c7c22d39bfd789796c77957a44c51b
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2025-01-31 16:42:50 +0000

    add local storage calls

---
+++ exhibition-curator/src/utils/local-storage-calls.js
@@ -0,0 +1,52 @@
+const STORAGE_KEY = "userExhibitions";
+
+// Get all exhibitions from local storage
+export const getExhibitions = () => {
+  const data = localStorage.getItem(STORAGE_KEY);
+  return data ? JSON.parse(data) : [];
+};
+
+
+// Save exhibitions to local storage
+export const saveExhibitions = (exhibitions) => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(exhibitions));
+};
+
+// Create a new exhibition
+export const createExhibition = (name) => {
+  const exhibitions = getExhibitions();
+  const newExhibition = { id: Date.now(), name, artworkIds: [] };
+  saveExhibitions([...exhibitions, newExhibition]);
+  return newExhibition;
+};
+
+// Add artwork to an exhibition
+export const addArtworkToExhibition = (exhibitionId, artworkId) => {
+  const exhibitions = getExhibitions();
+  const updatedExhibitions = exhibitions.map((exhibition) =>
+    exhibition.id === exhibitionId
+      ? { ...exhibition, artworkIds: [...exhibition.artworkIds, artworkId] }
+      : exhibition
+  );
+  saveExhibitions(updatedExhibitions);
+};
+
+// Remove artwork from an exhibition
+export const removeArtworkFromExhibition = (exhibitionId, artworkId) => {
+  const exhibitions = getExhibitions();
+  const updatedExhibitions = exhibitions.map((exhibition) =>
+    exhibition.id === exhibitionId
+      ? {
+          ...exhibition,
+          artworkIds: exhibition.artworkIds.filter((id) => id !== artworkId),
+        }
+      : exhibition
+  );
+  saveExhibitions(updatedExhibitions);
+};
+
+// Delete an exhibition
+export const deleteExhibition = (exhibitionId) => {
+  const exhibitions = getExhibitions().filter((exhibition) => exhibition.id !== exhibitionId);
+  saveExhibitions(exhibitions);
+};
