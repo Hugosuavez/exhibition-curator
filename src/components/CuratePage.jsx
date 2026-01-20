131729614dc3fc12c35843b92ac96868d6f46d40
Huw Livingstone <153728484+Hugosuavez@users.noreply.github.com>
2026-01-20 15:36:14 +0000

    remove scroll bars and clean up class and grid area names, begin to simplify grid

--- src/components/CuratePage.jsx
+++ src/components/CuratePage.jsx
@@ -38,17 +38,14 @@ export const CuratePage = () => {
   return (<>
         <Link to="/">Home</Link>
         <main className="curate-container">
+                <div className="home-button">Archivist!</div>
+                <div className="exhibition-title">{exhibition.name}</div>
             <section className="curate-preview">
-                <div className="one">{exhibition.name}</div>
-                <div className="two">two</div>
-                <div className="three">
-                  <CuratePreview artwork={artwork} setArtwork={setArtwork} exhibition={exhibition}/>
-                </div>
+                <CuratePreview artwork={artwork} setArtwork={setArtwork} exhibition={exhibition}/>
             </section>
+                <button className="met-link" onClick={() => handleToggle(true)}>Metropolitan Museum of Art</button>
+                <button className="harvard-link" onClick={() => handleToggle(false)}>Harvard Art Museums</button>
             <section className="curate-browse">
-                <button className="page-link" onClick={() => handleToggle(true)}>Metropolitan Museum of Art</button>
-              
-                <button className="page-link2" onClick={() => handleToggle(false)}>Harvard Art Museums</button>
                 <div className="browse-container">
                 {museumToggle ? <MetArtwork exhibition={exhibition} setArtwork={setArtwork} artwork={artwork}/> : <HarvardArtwork exhibition={exhibition} setArtwork={setArtwork} artwork={artwork}/>}
                 </div>
